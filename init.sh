83888806e5a8dc976a001d76a21c9a8f076e6dab
mihiai <mihai.nistor@sourcefabric.org>
2015-03-04 10:12:33 +0200

    fix: added konsole shortcuts and silent options for ini.sh

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
-sudo add-apt-repository ppa:alexx2000/doublecmd
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo add-apt-repository -y ppa:alexx2000/doublecmd > /dev/null 2>&1
+sudo add-apt-repository -y ppa:chris-lea/node.js > /dev/null 2>&1
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3 > /dev/null 2>&1
 
 wget -qO - https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo add-apt-repository "deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main"
+sudo add-apt-repository -y "deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main" > /dev/null 2>&1
 
 sudo sh -c "echo 'deb http://archive.canonical.com/ubuntu/ trusty partner' >> /etc/apt/sources.list.d/canonical_partner.list"
 
@@ -15,7 +15,7 @@ sudo echo "deb http://downloads.hipchat.com/linux/apt stable main" > \
 wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add -
 
 sudo apt-get update
-sudo apt-get install git git-cola google-chrome-stable skype nodejs sublime-text-installer mongodb elasticsearch redis-server
+sudo apt-get install doublecmd-qt git git-cola google-chrome-stable skype nodejs sublime-text-installer mongodb elasticsearch redis-server
 
 
 # Configure Elasticsearch to automatically start during bootup :
